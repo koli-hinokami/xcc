937d50645674b45dc359bdaf8e735441e163f6c0
koli-hinokami <koli_hinokami@mail.ru>
2024-06-01 20:27:07 +0500

    Plenty of tweaking

--- errorreportingfacilities.h
+++ errorreportingfacilities.h
@@ -1,48 +1,67 @@
 // Error reporiting facilities library
 
-#ifdef __CYGWIN__
-// Very dirty-hacked backtrace for Cygwin.
-// Based on my intimate knowledge of stackframing on x86 from x86-16 to amd64
-int backtrace(void** stacktracebuffer,int stacktracesize){
-	void** fp;
-	__asm__ __volatile__ (
-		 "movq %%rbp, %%rax"
-		:"=a"(fp)
-		:"a"(0)
-		:"memory"
-	);
-	int i;
-	for(;fp;i++){
-		printf("erf:[M] Backtracing: %p\n", fp);
-		fp=*fp;
-		stacktracebuffer[i]=fp;
-		if(i>=stacktracesize)return i;
-	};
-	return i;
-};
-#endif
+#include "lists.h"
+#ifndef __CYGWIN__
+#include "execinfo.h"
+#endif //  __CYGWIN__
 
+// -------------------- Types --------------------
+typedef struct {
+	char* string;
+} tErfVframe;
+// ------------------- Globals -------------------
+tList /* <tErfVframe> */ ErfVframes;
+
+
+
+void ErfDumpvframe(int count, tErfVframe* frame){
+	fprintf(stderr,"erf:[M]  • Native frame %p•{%s}\n",
+		frame,
+		frame->string
+	);
+};
 void ErfDumpstacktrace(){ // Stacktrace
-	fprintf(stderr,"erf:[M] Stacktrace:\n");
+	// Virtual stackframes - frames wound-up by program
+	fprintf(stderr,"erf:[M] Virtual stacktrace:\n");
+	int j=0;
+	for(tListnode* i=ErfVframes.first;i;i=i->next){
+		ErfDumpvframe(j,i->item);
+		j++;
+	};
+	//mtList_Foreach(&ErfVframes, (void(*)(void*))ErfDumpvframe);
+	fprintf(stderr,"erf:[M] (end virtualstacktrace)\n");
+#ifndef __CYGWIN__
+	fprintf(stderr,"erf:[M] Native stacktrace:\n");
 	int stacktracesize=60; // backtrace(nullptr,0);
 	void* stacktracebuffer[stacktracesize];
 	stacktracesize = backtrace(stacktracebuffer,stacktracesize);
-	char** stacktracesymbols = nullptr; // backtrace_symbols(stacktracebuffer,stacktracesize);
+	char** stacktracesymbols = backtrace_symbols(stacktracebuffer,stacktracesize);
 	for(int i=0;i<stacktracesize;i++){
 		fprintf(stderr,"erf:[M]  • %i: %p∙%s\n",
 			i,
 			stacktracebuffer[i],
-			(char*)nullptr // stacktracesymbols[i]
+			stacktracesymbols?stacktracesymbols[i]:nullptr
 		);
 	};
+	fprintf(stderr,"erf:[M] (end nativestacktrace)\n");
+#endif
 };
 void ErfError(){
 	fprintf(stderr,"erf:[E] Error occured!\n");
 	ErfDumpstacktrace();
-	fprintf(stderr,"erf:[E] (end)\n");
+	fprintf(stderr,"erf:[E] (end errorlog)\n");
+	exit(1);
 };
 void ErfFatal(){
 	fprintf(stderr,"erf:[F] Fatal error occured!\n");
 	ErfDumpstacktrace();
-	fprintf(stderr,"erf:[F] (end)\n");
+	fprintf(stderr,"erf:[F] (end errorlog)\n");
+	exit(1);
+};
+void* ErfCatchnullptr(void* self){
+	if(!self){
+		fprintf(stderr,"erf:[F] ErfCatchnullptr: Nullpointer dereference caught!\n");
+		ErfFatal();
+	};
+	return self;
 };
