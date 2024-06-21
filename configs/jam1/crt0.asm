7d2a299b12b10636d0f0026ae354d2429e8c1ce8
koli-hinokami <koli_hinokami@mail.ru>
2024-06-21 10:07:02 +0500

    Refactor libc and crt0

---
+++ configs/jam1/crt0.asm
@@ -0,0 +1,33 @@
+; vim:ts=8
+	; Jam-1 crt0
+	; (the code executed when program starts. be careful here)
+_start:	push	ra
+	mov	si,	0
+	mov	ab,	__libc_envp
+	push	b
+	push	a
+	mov	ab,	__libc_argv
+	push	b
+	push	a
+	mov	ab,	1
+	push	b
+	push	a
+	call	main
+	nop
+	pop	ra
+	nop
+	ret
+__libc_argv:	
+	dw	__libc_argv_0
+__libc_envp:	
+	dw	0
+__libc_argv_0:
+	db	106
+	db	97
+	db	109
+	db	49
+	db	112
+	db	116
+	db	111
+	db	103
+	db	0
---
+++ configs/jam1/libc.asm
@@ -0,0 +1,3 @@
+; vim:ts=8:
+	; Jam-1 Runtime library
+	; (add libc functions here)
