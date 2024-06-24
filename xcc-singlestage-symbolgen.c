66280cee1344e7c424dd3bb1bd179cafa506496b
koli-hinokami <koli_hinokami@mail.ru>
2024-06-24 10:50:46 +0500

    Fix SgResolveunresolvedtypes_Resolvetype resetting value category

--- xcc-singlestage-symbolgen.c
+++ xcc-singlestage-symbolgen.c
@@ -555,12 +555,14 @@ tGType* SgResolvetype(tGNamespace* name_space, char* name){
 };
 void SgResolveunresolvedtypes_Resolvetype(tGType* type){
 	printf("SG: [T] SgResolveunresolvedtypes_Resolvetype: Resolving type \"%s\"\n",type->unresolvedsymbol);
+	eGValuecategory valcat=type->valuecategory;
 	tGType* temp = SgResolvetype(GRootnamespace,type->unresolvedsymbol);
 	if(temp==nullptr){
 		fprintf(stderr,"SG: [E] SgResolveunresolvedtypes_Resolvetype: Unable to resolve type \"%s\"! \n",type->unresolvedsymbol);
 		ErfError();
 	}else{
 		*type = *temp;
+		type->valuecategory=valcat;
 	}
 };
 void SgResolveunresolvedtypes(){
