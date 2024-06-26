d00cef2f46c90072cb21b1f305f0a54f04c54762
koli-hinokami <koli_hinokami@mail.ru>
2024-06-26 07:04:35 +0500

    Move math subroutines into separate files `fixed16.h`/`math.asm`

--- configs/jam1/libc.h
+++ configs/jam1/libc.h
@@ -1,5 +1,3 @@
 
 extern void puts(char* str);
 extern void CliWriteinteger(uint16_t num);
-extern fixed16 fixed16_divide(fixed16 numerator, fixed16 denominator);
-extern fixed16 fixed16_reciprocal(fixed16 num);
